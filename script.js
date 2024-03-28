854882d73d809940b15176d0c8520fdd009ce7d9
briansomogyi <brian.somogyi@emanuel.ro>
2024-03-28 10:55:12 +0200

    Am modificat fisierul 'script.js', astfel incat sa pot lucra cu obiecte

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function sumaMatrice(matrice1, matrice2) {
-    if (typeof matrice1 == "object" && typeof matrice2 == "object") {
-        let matrice3 = []
-        for (let index = 0; index < matrice1.length; index++) {
-            matrice3.push([])
-            for (let jindex = 0; jindex < matrice1[index].length; jindex++) {
-                matrice3[index].push(matrice1[index][jindex] + matrice2[index][jindex])
 
-            }
+    let matrice3 = []
+    for (let index = 0; index < matrice1.length; index++) {
+        matrice3.push([])
+        for (let jindex = 0; jindex < matrice1[index].length; jindex++) {
+            matrice3[index].push({ brian: matrice1[index][jindex].brian + matrice2[index][jindex].brian })
 
         }
-        return matrice3
-    } else {
-        console.log("Suma nu a putut fi efectuata.")
-        return []
+
     }
+    return matrice3
+
 }
 
-let matrice1 = [[14, 15], [16, 17], [18, 19]]
-let matrice2 = [[20, 21], [22, 23], [24, 25]]
+let matrice1 = [[{ brian: 14 }, { brian: 15 }], [{ brian: 16 }, { brian: 17 }], [{ brian: 18 }, { brian: 19 }]]
+let matrice2 = [[{ brian: 20 }, { brian: 21 }], [{ brian: 22 }, { brian: 23 }], [{ brian: 24 }, { brian: 25 }]]
 console.log(sumaMatrice(matrice1, matrice2))
